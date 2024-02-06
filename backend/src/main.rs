fd528c8cb45a489468064224346de05861c09699
Mahmoud Rawas <mahmoud.rawas@gmail.com>
2024-02-06 19:26:48 +1100

    create an option to connect into a test database for test builds

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 use sqlx::pool::Pool;
+use sqlx::PgPool;
 use tide::Server;
 mod test;
 mod controllers;
@@ -7,14 +8,28 @@ mod common;
 use common::error::Error;
 use common::state::State;
 use controllers::index::index;
+
+#[cfg(not(test))]
+async fn make_db_pool()->Result<PgPool, Error>{
+    let db_url = std::env::var("DATABASE_URL")?;
+    Ok(Pool::connect(&db_url).await?)
+}
+
+#[cfg(test)]
+async fn make_db_pool() -> Result<PgPool, Error> {
+    let db_url = std::env::var("DATABASE_URL_TEST")?;
+    // let pool = Pool::connect(&db_url).await?;
+    // Ok(pool)
+    Ok(Pool::connect(&db_url).await?)
+}
+
 #[async_std::main]
 async fn main() -> Result<(), Error> {
     dotenv::dotenv().ok();
     pretty_env_logger::init();
 
-    let db_url = std::env::var("DATABASE_URL")?;
-
-    let db_pool = Pool::connect(&db_url).await?;
+    
+    let db_pool = make_db_pool().await?;
 
     let mut app: Server<State> = Server::with_state(State { db_pool });
 
